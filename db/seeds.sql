2b256ca8514d667b70aa0d3b9701a71aae62d3b9
Wilder8 <jacob.wilder826@gmail.com>
2019-10-30 17:06:40 -0400

    Added JAWSDB addon for heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,4 @@ VALUES
   ("Pick up firewood", "Hosting bonfire on Friday, November 1st, 7:00 P.M"),
   ("Mark myself present in Bootcampspot", "Remember to mark myself present every Monday-Friday at 10am"),
   ("Commit more", "Get those commits up. It shows the process and progress."),
-  ("Study for Project 2", "Project 2 platformer game WIP/Remember to study!")
+  ("Study for Project 2", "Project 2 platformer game WIP/Remember to study!");
