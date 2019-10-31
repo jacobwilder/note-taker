8f3a485115bce2692b3d160934c615c9796979e2
Wilder8 <jacob.wilder826@gmail.com>
2019-10-30 21:03:26 -0400

    Updated .env file

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
 var mysql = require("mysql");
-
+var dotenv = require("dotenv");
 var connection;
+
+dotenv.config();
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root", // FIXME: Establish a connection to your MySQL Database
-    password: "26Gamers8!", // https://www.npmjs.com/package/mysql#establishing-connections
+    user: process.env.DB_USER, // FIXME: Establish a connection to your MySQL Database
+    password: process.env.DB_PW, // https://www.npmjs.com/package/mysql#establishing-connections
     database: "note_db"
   });
 }
